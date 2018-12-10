a32d2d364beb94704b3861cd57b7723667e6b450
drillprop <drylbartosz@gmail.com>
2018-12-10 19:04:50 +0100

    remove plugins, i dont need them, update babelrc

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 
 const root = document.getElementById('root');
 
-const App = () => {
-  return <h1>Hello</h1>;
-};
+class App extends React.Component {
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
 
 ReactDOM.render(<App />, root);
