e2e61135632c50988d04f19ba7aa948a814fe9b1
drillprop <drylbartosz@gmail.com>
2018-12-11 17:50:22 +0100

    reduce lines of code, remove unused variables

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 
 class Currency extends Component {
-  state = {
-    value: 0
-  };
-
   handleChange = e => {
     const { value } = e.currentTarget;
     this.setState({ value: value * 4.2 });
   };
 
   render() {
-    const { children } = this.props;
-    const { value } = this.state;
-    return children(this.handleChange, value);
+    const { children, amount } = this.props;
+    return children(amount);
   }
 }
 
