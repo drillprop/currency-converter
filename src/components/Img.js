f97b1b7dcf6bdb6b691d441bbaa6c4167f956512
drillprop <drylbartosz@gmail.com>
2018-12-12 21:37:13 +0100

    change czk image, add switch statement to Image

--- src/components/Img.js
+++ src/components/Img.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import usd from '../assets/usd.jpg';
+import eur from '../assets/eur.jpg';
+import czk from '../assets/czk.jpg';
 
 const Image = styled.img`
   max-height: 100vh;
 `;
 
-const Img = ({ img }) => <Image src={img} />;
+const Img = ({ currency }) => {
+  switch (currency) {
+    case 'usd':
+      return <Image src={usd} />;
+    case 'eur':
+      return <Image src={eur} />;
+    case 'czk':
+      return <Image src={czk} />;
+  }
+};
 
 export default Img;
