8b4d513d7c183ff9fbafbe08e2d843f992c5fa30
drillprop <drylbartosz@gmail.com>
2018-12-13 22:06:04 +0100

    apply sanserif to body

--- src/utilities/GlobalStyle.js
+++ src/utilities/GlobalStyle.js
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
+import { sansSerif } from './fonts';
 
 export default createGlobalStyle`
 @import url('https://fonts.googleapis.com/css?family=Montserrat:400|Noto+Serif+TC:300,400,600');
 body {
   margin:0;
+  font-family: ${sansSerif};
+  font-size:16px;
 }
 
 `;
