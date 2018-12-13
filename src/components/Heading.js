e8428c34eb5723583b95f6ad88c512722de11e8e
drillprop <drylbartosz@gmail.com>
2018-12-13 21:50:55 +0100

    apply some font styles to headings

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import { serif, sansSerif } from '../utilities/fonts';
 
-const Title = styled.h1``;
-const SubTitle = styled.h3``;
+const Title = styled.h1`
+  font-size: 40px;
+  font-family: ${serif};
+  font-weight: 700;
+`;
+const SubTitle = styled.h3`
+  font-size: 16px;
+  font-family: ${sansSerif};
+  font-weight: 400;
+  color: #303030;
+`;
 
 const Heading = () => (
   <header>
