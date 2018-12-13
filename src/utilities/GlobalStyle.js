3093906d7a4024f2195aebb58dcc01d6295faab8
drillprop <drylbartosz@gmail.com>
2018-12-13 21:50:26 +0100

    import google fonts

--- src/utilities/GlobalStyle.js
+++ src/utilities/GlobalStyle.js
@@ -1,7 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400|Noto+Serif+TC:300,400,600');
 body {
   margin:0;
 }
+
 `;
