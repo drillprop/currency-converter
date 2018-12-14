b1cee7093a1f6125d25456fd34ec35ccdb3c37b9
drillprop <drylbartosz@gmail.com>
2018-12-14 22:21:48 +0100

    style input and select - fonts, alignment in general

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Currency from './Currency';
 import Heading from './Heading';
+import { serif, sansSerif } from '../utilities/fonts';
 
 const StyledMain = styled.main`
   margin-top: 50px;
   margin-left: 100px;
+`;
+const StyledInput = styled.input`
+  margin: 0;
+  font-size: 16px;
+  border: none;
+  border-bottom: 1px solid black;
+  font-family: ${sansSerif};
+  margin-left: 40px;
+  width: 220px;
+  height: 25px;
+`;
+const StyledSelect = styled.select`
+  margin: 0;
+  border: none;
+  border-bottom: 1px solid black;
+  height: 27px;
+  font-family: ${sansSerif};
+  font-size: 16px;
 `;
 
 class Main extends Component {
@@ -14,14 +33,14 @@ class Main extends Component {
     return (
       <StyledMain>
         <Heading />
-        <input type='number' onChange={getAmount} />
-        <select onChange={getCurrency}>
+        <StyledInput type='number' onChange={getAmount} />
+        <StyledSelect onChange={getCurrency}>
           <option value='usd'>usd</option>
           <option value='eur'>eur</option>
           <option value='czk'>czk</option>
-        </select>
+        </StyledSelect>
         <Currency amount={amount} currency={currency}>
-          {value => <div>{value}</div>}
+          {value => <div style={{ display: 'inline' }}>{value} pln</div>}
         </Currency>
       </StyledMain>
     );
