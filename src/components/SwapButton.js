e9139f10f1e105a55c498ce422495c987ea3fb79
drillprop <drylbartosz@gmail.com>
2018-12-23 17:13:58 +0100

    change style

--- src/components/SwapButton.js
+++ src/components/SwapButton.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import { serif, sansSerif } from '../utilities/fonts';
+import { sansSerif } from '../utilities/fonts';
 
 const Button = styled.button`
+  align-self: center;
   display: block;
   font-family: ${sansSerif};
-  margin-top: 20px;
+  margin-top: 30px;
   background: #444;
+  text-transform: uppercase;
   border: none;
   padding: 10px 20px;
   color: white;
