d4d57de443538d7b6eed5ee3bab4a4984872902d
drillprop <drylbartosz@gmail.com>
2018-12-25 16:05:50 +0100

    add getCurrency method, add pln props

--- src/components/PlnCurrency.js
+++ src/components/PlnCurrency.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Select from './Select';
 import Currency from './Currency';
 
-const PlnCurrency = ({ amount, currency }) => (
-  <Currency amount={amount} currency={currency}>
+const PlnCurrency = ({ amount, currency, getCurrency }) => (
+  <Currency pln amount={amount} currency={currency}>
     {value => (
-      <div style={{ display: 'inline', color: '#666' }}>equals {value}</div>
+      <div style={{ display: 'inline', color: '#666' }}>
+        PLN equals {value} <Select getCurrency={getCurrency} />
+      </div>
     )}
   </Currency>
 );
