0c34b72dc72fa39ad3642f270d624786b07e8620
drillprop <drylbartosz@gmail.com>
2018-12-25 18:17:47 +0100

    add spring animation on Aside

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -2,11 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import Img from './Img';
 const StyledAside = styled.aside``;
+import { Spring } from 'react-spring';
 
 const Aside = ({ currency }) => (
-  <StyledAside>
-    <Img currency={currency} />
-  </StyledAside>
+  <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+    {props => (
+      <StyledAside style={props}>
+        <Img currency={currency} />
+      </StyledAside>
+    )}
+  </Spring>
 );
 
 export default Aside;
