c16b64344f57c21890f0c5664dd4bf92f3557830
drillprop <drylbartosz@gmail.com>
2019-01-03 19:30:45 +0100

    reorganize App, remove previous components - Container and Main, leave just GlobalStyle and Navigation, wrap Navigation in BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import Main from './Main';
 import GlobalStyle from '../utilities/GlobalStyle';
 import Navigation from './Navigation';
-
-const Container = styled.div`
-  height: 100vh;
-  display: grid;
-  grid-template-columns: 0.75fr 1fr 0.5fr;
-`;
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <>
-        <Navigation />
-        <Container>
-          <GlobalStyle />
-          <Main />
-        </Container>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Navigation />
+        </BrowserRouter>
       </>
     );
   }
