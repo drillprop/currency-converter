1e8d5ab541b233eb55dc4cdac2daef2bb87e76cd
drillprop <drylbartosz@gmail.com>
2019-01-03 13:12:06 +0100

    style nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Nav = styled.nav`
+  color: #444;
   position: absolute;
   top: 0;
   right: 0;
   margin-top: 10px;
   margin-right: 50px;
+  text-transform: uppercase;
 `;
 
 const Ul = styled.ul`
@@ -17,13 +19,21 @@ const Li = styled.li`
   list-style: none;
   margin-left: 20px;
   font-size: 13px;
+  a {
+    text-decoration: none;
+    color: #444;
+  }
 `;
 
 const Navigation = () => (
   <Nav>
     <Ul>
-      <Li>Show Current Rates</Li>
-      <Li>Show Rates from last 7 days</Li>
+      <Li>
+        <a href=''>Current exchange Rates</a>
+      </Li>
+      <Li>
+        <a href=''>Rates from last 7 days</a>
+      </Li>
     </Ul>
   </Nav>
 );
