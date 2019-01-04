78730058fc58a32db30e81c84d4669082431ee84
drillprop <drylbartosz@gmail.com>
2019-01-04 20:41:56 +0100

    modify to PureComponent

--- src/utilities/Currency.js
+++ src/utilities/Currency.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Loading from '../elements/Loading';
 
-class Currency extends Component {
+class Currency extends PureComponent {
   state = {
     rate: 0
   };
