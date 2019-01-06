c1a88890a7a8a68bf9729c0c51431d77c55ece59
drillprop <drylbartosz@gmail.com>
2019-01-06 22:04:01 +0100

    create component which will be collecting rates data from 100 last days

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { getPastDates, getTodayDate } from '../utilities/helpers';
 
 class Charts extends Component {
-  state = { rate: {} };
+  state = { dates: [] };
+  componentDidMount = () => {
+    const currency = 'usd';
+    const startDate = getPastDates(100);
+    const endDate = getTodayDate();
+    const url = `http://api.nbp.pl/api/exchangerates/rates/a/${currency}/${startDate}/${endDate}/`;
+    fetch(url)
+      .then(data => data.json())
+      .then(json => this.setState({ dates: json.rates.reverse() }));
+  };
+
   render() {
-    return <div>Here, will be charts</div>;
+    const { dates } = this.state;
+    return <div>hello</div>;
   }
 }
 
