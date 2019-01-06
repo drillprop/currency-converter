6d7c4056d0da8aa4488c41040c35f1002c9aa15c
drillprop <drylbartosz@gmail.com>
2019-01-06 12:29:24 +0100

    Add Charts, and ChartContainer

---
+++ src/components/ChartContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Charts from './Charts';
+import { Container } from '../elements/Container';
+
+const ChartContainer = () => (
+  <Container>
+    <Charts />
+  </Container>
+);
+
+export default ChartContainer;
---
+++ src/components/Charts.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Charts extends Component {
+  state = { rate: {} };
+  render() {
+    return <div>Here, will be charts</div>;
+  }
+}
+
+export default Charts;
