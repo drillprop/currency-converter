ce89a0fcdf60356ff74f58827cb4231b7cc08d40
drillprop <drylbartosz@gmail.com>
2019-01-06 18:17:33 +0100

    convert getDates to function which takes number as an argument and returns date number days ago as a value

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,14 +1,11 @@
 const getDates = num => {
-  let datesArray = [];
   const now = new Date();
   const getTodayDate = now.getDate();
-  for (let i = 0; i <= num; i++) {
-    console.log(i);
-    let getDayBefore = now.setDate(getTodayDate - i);
-    let convertNewDate = new Date(getDayBefore).toISOString().slice(0, 10);
-    datesArray.push(convertNewDate);
-  }
-  return datesArray;
+
+  let getDayBefore = now.setDate(getTodayDate - num);
+  let convertNewDate = new Date(getDayBefore).toISOString().slice(0, 10);
+
+  return convertNewDate;
 };
 
 export { getDates };
