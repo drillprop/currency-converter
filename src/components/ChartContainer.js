0d3b00d6d1c56a1057449435f23a734610d9cd56
drillprop <drylbartosz@gmail.com>
2019-01-09 13:11:41 +0100

    add aside to chartcontainer

--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Charts from './Charts';
 import { Container } from '../elements/Container';
+import Aside from './Aside';
 
 const ChartContainer = () => (
   <Container>
+    <Aside />
     <Charts />
   </Container>
 );
