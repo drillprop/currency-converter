d722bbb219ecdd101b6bee91d10fcfe0b4c4353f
drillprop <drylbartosz@gmail.com>
2019-01-09 11:53:26 +0100

    create chart using recharts, add error and loading handlers

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,21 +1,74 @@
 import React, { Component } from 'react';
 import { getPastDates, getTodayDate } from '../utilities/helpers';
+import {
+  AreaChart,
+  LineChart,
+  Area,
+  Line,
+  YAxis,
+  XAxis,
+  CartesianGrid
+} from 'recharts';
+import Loading from '../elements/Loading';
+
+const CustomizedAxisTick = ({ payload, x, y }) => {
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text x={10} y={0} dy={16} textAnchor='end' transform='rotate(-35)'>
+        {payload.value}
+      </text>
+      ;
+    </g>
+  );
+};
 
 class Charts extends Component {
-  state = { dates: [] };
+  state = { dates: [], error: false };
   componentDidMount = () => {
-    const currency = 'usd';
-    const startDate = getPastDates(100);
+    const currency = 'eur';
+    const startDate = getPastDates(350);
     const endDate = getTodayDate();
     const url = `http://api.nbp.pl/api/exchangerates/rates/a/${currency}/${startDate}/${endDate}/`;
     fetch(url)
       .then(data => data.json())
-      .then(json => this.setState({ dates: json.rates.reverse() }));
+      .then(json => this.setState({ dates: json.rates }))
+      .catch(error => this.setState({ error }));
   };
 
   render() {
-    const { dates } = this.state;
-    return <div>hello</div>;
+    const { dates, error } = this.state;
+    if (error) {
+      return null;
+    } else if (!dates.length) {
+      return <Loading />;
+    } else {
+      return (
+        <>
+          <AreaChart
+            width={500}
+            height={500}
+            data={dates}
+            margin={{ top: 20, right: 30, left: 20, bottom: 50 }}>
+            <Area
+              type='monotone'
+              dataKey='mid'
+              stroke='#82ca9d'
+              fill='#82ca9d'
+              fillOpacity={0.6}
+            />
+            {/* <Line dot={false} type='monotone' dataKey='mid' stroke='#8884d8' /> */}
+            <YAxis dataKey='mid' type='number' domain={[3, 5]} />
+            <CartesianGrid stroke='#eee' />
+            <XAxis
+              interval={30}
+              type='category'
+              dataKey='effectiveDate'
+              tick={<CustomizedAxisTick />}
+            />
+          </AreaChart>
+        </>
+      );
+    }
   }
 }
 
