0b5f965ccab8bb0f67e76762a26a04033a466f23
drillprops <drylbartosz@gmail.com>
2020-04-28 21:57:42 +0200

    wrap with Suspense only charts Route

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,19 +2,19 @@ import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Loading from '../elements/Loading';
 import Home from './Home';
+import Rates from './Rates';
 
 const Charts = lazy(() => import('./Charts'));
-const Rates = lazy(() => import('./Rates'));
 
 const Router = () => {
   return (
-    <Suspense fallback={<Loading />}>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/rates' component={props => <Rates {...props} />} />
-        <Route path='/charts' component={props => <Charts {...props} />} />
-      </Switch>
-    </Suspense>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/rates' component={(props) => <Rates {...props} />} />
+      <Suspense fallback={<Loading />}>
+        <Route path='/charts' component={(props) => <Charts {...props} />} />
+      </Suspense>
+    </Switch>
   );
 };
 
